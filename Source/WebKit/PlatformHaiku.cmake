a0f1130135dac8e38296672e2c9e417af735ca11
PulkoMandy <pulkomandy@pulkomandy.tk>
2023-03-11 19:19:46 +0100

    WebKit/PlatformHaiku: add missing include directories and cross-platform files

--- Source/WebKit/PlatformHaiku.cmake
+++ Source/WebKit/PlatformHaiku.cmake
@@ -12,8 +12,12 @@ list(APPEND WebKit_SOURCES
     NetworkProcess/haiku/NetworkSessionHaiku.cpp
     NetworkProcess/haiku/NetworkDataTaskHaiku.cpp
 
+    NetworkProcess/Classifier/WebResourceLoadStatisticsStore.cpp
+
     Platform/IPC/haiku/ConnectionHaiku.cpp
     Platform/IPC/haiku/IPCSemaphoreHaiku.cpp
+
+    Platform/classifier/ResourceLoadStatisticsClassifier.cpp
 
     Platform/classifier/ResourceLoadStatisticsClassifier.cpp
 
@@ -68,6 +72,8 @@ list(APPEND WebKit_INCLUDE_DIRECTORIES
     "${DERIVED_SOURCES_HAIKU_API_DIR}"
     "${WEBKIT_DIR}/NetworkProcess/unix"
     "${WEBKIT_DIR}/Platform"
+    "${WEBKIT_DIR}/Platform/classifier"
+    "${WEBKIT_DIR}/Platform/generic"
     "${WEBKIT_DIR}/Platform/IPC/unix"
     "${WEBKIT_DIR}/Shared/API/c/haiku"
     "${WEBKIT_DIR}/Shared/CoordinatedGraphics"
